14a71fa16665c0741c67a602f5edc2dc3dc92da5
herebird <35319775+herebird@users.noreply.github.com>
2018-02-26 20:50:21 +0700

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,3 +1,38 @@
+#!/bin/bash
+myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
+
+ red='\e[1;31m'
+               green='\e[0;32m'
+               NC='\e[0m'
+			   
+               echo "Connect ocspanel.info..."
+               sleep 1
+               
+			   echo "กำลังตรวจสอบ Permision..."
+               sleep 1
+               
+			   echo -e "${green}ได้รับอนุญาตแล้ว...${NC}"
+               sleep 1
+			   
+flag=0
+
+if [ $USER != 'root' ]; then
+	echo "คุณต้องเรียกใช้งานนี้เป็น root"
+	exit
+fi
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+
+if [[ -e /etc/debian_version ]]; then
+	#OS=debian
+	RCLOCAL='/etc/rc.local'
+else
+	echo "คุณไม่ได้เรียกใช้สคริปต์นี้ในระบบปฏิบัติการ Debian"
+	exit
+fi
 
 #INDEX.PHP
 mkdir -p /var/www/html/Server
