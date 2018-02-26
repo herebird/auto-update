1f7cece8c5b8d76e1fecbbf3e680b87e5f4f1d1e
herebird <35319775+herebird@users.noreply.github.com>
2018-02-26 21:13:56 +0700

    Update x.sh

--- x.sh
+++ x.sh
@@ -39,26 +39,15 @@ cd
 
 #INDEX.PHP
 mkdir -p /var/www/html/Server
-echo "#www.ocspanel.info - Facebook.com/ceolnw
-<?php
-//open connection to mysql db
-	$connection = mysqli_connect("localhost","admin","123456789","server") or die("Error" .mysqli_error($connection));
 
-//fetch table rows from mysql db
-	$sql = "select * from table1";
-	$result =  mysqli_query($connection, $sql) or die("Error in Selecting" .mysqli_error($connection));
+# GO TO ROOT
+cd /var/www/html/Server
 
-//create an array
-	$emparray = array();
-	while($row =mysqli_fetch_assoc($result))
-	{
-		$emparray[] = $row;
-	}
-	echo json_encode($emparray);
+# Dowload
 
-//close the db connection
-	mysqli_close($connection);
-?>" >/var/www/html/Server/index.php
+
+
+herebird/auto-update/blob/master/index.php
 
 #แก้ไขข้อมูลการเชื่อมต่อฐานข้อมูล phpmyadmin
 nano /var/www/html/Server/index.php
